cd2ac0f9677248102fbdced8d17ad5683761079a
Preet Acharya <acharyapreet2@gmail.com>
2025-01-02 20:33:31 +0530

    added context api

---
+++ src/context/TodoContext.js
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+// const TodoContext = createContext(null);
+// export default todoContext;
+
+const todoContext =createContext(null); // Provide a default value (null in this case)
+
+export default todoContext;
